ac05108a60dd51f775772ba7f69bedbc10b02db3
AdrianHouse <72535167+eydrien@users.noreply.github.com>
2025-06-02 21:10:28 -0500

    Revert "Hompage and students"
    This reverts commit ab9f7d7370d495f18eb2a1e5502c623f41b96461.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import EstudiantesPage from './pages/EstudiantesPage'
-import DashboardPage from './pages/DashboardPage'
-
+// Aquí importarás más páginas a medida que las crees
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/estudiantes" element={<EstudiantesPage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
-      
+      {/* Aquí más rutas */}
     </Routes>
   )
 }
